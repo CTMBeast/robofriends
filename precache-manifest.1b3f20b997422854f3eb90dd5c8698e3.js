7750d0b0f53fe1748ca35f9e90aafcac7f0d39d6
petregabriel04 <58558927+petregabriel04@users.noreply.github.com>
2020-01-18 23:54:10 +0200

    Updates

---
+++ precache-manifest.1b3f20b997422854f3eb90dd5c8698e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c07b7cae297fd38bc150bd1e3028981",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c840c1ed80f435b134c1",
+    "url": "/robofriends/static/css/main.c6f4f84d.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c840c1ed80f435b134c1",
+    "url": "/robofriends/static/js/main.6b83f9ba.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
